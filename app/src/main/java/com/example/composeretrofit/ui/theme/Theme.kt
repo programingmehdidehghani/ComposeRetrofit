7190b55c915c50dae6fb4290b887e3a2f9fb788b
mehdi <dehghani_m@rocketmail.com>
2022-06-30 20:19:39 +0430

    first commit

--- app/src/main/java/com/example/composeretrofit/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeretrofit/ui/theme/Theme.kt
@@ -5,18 +5,10 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+
 
 private val DarkColorPalette = darkColors(
     primary = Color.Yellow,
