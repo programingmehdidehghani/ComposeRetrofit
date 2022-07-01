18fc2e07e038c452713c49e4b85d304dfb4d1c57
mehdi <dehghani_m@rocketmail.com>
2022-07-01 14:00:10 +0430

    try make ui data

--- app/src/main/java/com/example/composeretrofit/MainActivity.kt
+++ app/src/main/java/com/example/composeretrofit/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.composeretrofit.data.remote.response.PokemoneList
+import com.example.composeretrofit.pokemonList.PokemonListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -25,7 +27,7 @@ class MainActivity : ComponentActivity() {
                  , startDestination = "pokemon_list_screen"
              ){
                  composable("pokemon_list_screen"){
-
+                     PokemonListScreen(navController = navController)
                  }
                  composable(
                      "pokemon_detail_screen/{dominantColor}/{pokemonName}",
