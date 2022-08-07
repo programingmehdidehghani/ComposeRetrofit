5fadbecb93c355d02a587a0b7a5239cd41a00995
mehdi <dehghani_m@rocketmail.com>
2022-08-07 14:38:37 +0430

    make package project

--- app/src/main/java/com/example/composeretrofit/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeretrofit/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ComposeRetrofitTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun ComposeRetrofitTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
