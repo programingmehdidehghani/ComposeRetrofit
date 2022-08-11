bf3e0803217a7d87218d9a88edf459dcee16b454
mehdi <dehghani_m@rocketmail.com>
2022-08-11 14:54:23 +0430

    continue make ui

--- app/src/main/java/com/example/composeretrofit/pokemonList/PokemonListScreen.kt
+++ app/src/main/java/com/example/composeretrofit/pokemonList/PokemonListScreen.kt
@@ -8,27 +8,35 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.NavController
+import coil.request.ImageRequest
 import com.example.composeretrofit.R
 import com.example.composeretrofit.data.models.PokedexListEntry
+import com.example.composeretrofit.ui.theme.RobotoCondensed
 import com.google.accompanist.coil.CoilImage
 
 
@@ -137,8 +145,60 @@ fun PokedexEntry(
             }
     ){
         Column {
-
+            CoilImage(
+                request = ImageRequest.Builder(LocalContext.current)
+                    .data(entry.imageUrl)
+                    .target{
+                        viewModel.calDominantColor(it){ color ->
+                            dominantColor = color
+                        }
+                    }
+                    .build(),
+                contentDescription = entry.pokemonName,
+                fadeIn = true,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            ) {
+                CircularProgressIndicator(
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier.scale(0.5f)
+                )
+            }
+            Text(
+                text = entry.pokemonName,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
+    }
+}
+
+@Composable
+fun PokedexRow(
+    rowIndex : Int,
+    entries : List<PokedexListEntry>,
+    navController: NavController
+){
+    Column {
+        Row {
+            PokedexEntry(entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= rowIndex *2 + 2){
+                PokedexEntry(entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            }else{
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
