f77bd3188640f385ccd75fbb3307313d4b927113
mehdi <dehghani_m@rocketmail.com>
2022-08-11 15:16:42 +0430

    continue try

--- app/src/main/java/com/example/composeretrofit/pokemonList/PokemonListViewModel.kt
+++ app/src/main/java/com/example/composeretrofit/pokemonList/PokemonListViewModel.kt
@@ -3,17 +3,47 @@ package com.example.composeretrofit.pokemonList
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import com.example.composeretrofit.data.models.PokedexListEntry
 import com.example.composeretrofit.repository.PokemonRepository
+import com.example.composeretrofit.util.Constants.PAGE_SIZE
+import com.example.composeretrofit.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
-    private val pokemonRepository: PokemonRepository
+    private val repository: PokemonRepository
 ) : ViewModel() {
+
+    private var curPage = 0
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    fun loadPokemonPaginated(){
+        viewModelScope.launch {
+            isLoading.value = true
+            val result = repository.getPokemonList(PAGE_SIZE,curPage * PAGE_SIZE)
+            when(result){
+                is Resource.Success -> {
+                     endReached.value = curPage * PAGE_SIZE >= result.data!!.count
+                     val pokedexEntries = result.data.results.mapIndexed{ index, entry ->
+                         val number 
+                     }
+                }
+                is Resource.Error -> {
+
+                }
+            }
+        }
+    }
 
     fun calDominantColor(drawable: Drawable, onFinish: (Color) -> Unit){
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888,true)
